82f31740ffea00e416b5ed51cea899216365df47
Weiran Li <weiran@second.dev>
2023-08-11 12:43:59 -0700

    Second[Agent] mjml to react email

---
+++ AccordionTitle.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+const AccordionTitle = () => {
+  return (
+    <Container>
+      <Section>
+        <Row>
+          <Column>
+            <Heading>Accordion Title</Heading>
+          </Column>
+        </Row>
+      </Section>
+    </Container>
+  );
+};
+
+export default AccordionTitle;
---
+++ Class.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+const Class = () => {
+  return <>{/* Your Class component code here */}</>;
+};
+
+const mjClassToReactComponent = (mjClass) => {
+  if (!mjClass) {
+    return "";
+  }
+
+  return <Class />;
+};
+
+export default mjClassToReactComponent;
