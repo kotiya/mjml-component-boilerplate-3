acf300d67f70a0361a6aab86449954f3d8fea8b1
root <root@ip-10-0-1-43.us-west-1.compute.internal>
2023-08-18 06:26:39 +0000

    Second[Agent] mjml to react email

---
+++ Accordion.tsx
@@ -0,0 +1,44 @@
+// Friendly Reminder: This snippet was produced with the assistance of an AI. Always double-check for your specific requirements.
+import React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+const Accordion = () => {
+  return (
+    <Section>
+      <Head>
+        <Font />
+        <Html />
+        <Body>
+          <Container>
+            <Row>
+              <Column>
+                <Heading>Accordion</Heading>
+                <Hr />
+                {/* Add your accordion content here */}
+              </Column>
+            </Row>
+          </Container>
+        </Body>
+      </Head>
+    </Section>
+  );
+};
+
+export default Accordion;
