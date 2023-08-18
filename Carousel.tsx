dc45bf72d670fc9863cd9deb5dfcfa7cf57e1b84
root <root@ip-10-0-0-251.us-west-1.compute.internal>
2023-08-18 05:31:10 +0000

    Second[Agent] mjml to react email

---
+++ Carousel.tsx
@@ -0,0 +1,34 @@
+// Friendly Reminder: This snippet was produced with the assistance of an AI. Always double-check for your specific requirements.
+import React from "react";
+import {
+  Body,
+  Button,
+  Column,
+  Container,
+  Font,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+const Carousel = () => {
+  return (
+    <Section>
+      <Head>
+        <Font href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+        <Font href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
+      </Head>
+      <Body>{/* Your carousel content goes here */}</Body>
+    </Section>
+  );
+};
+
+export default Carousel;
